e4d1820390832858089bb1b2bc7b0bc743f36d37
Manuel Hoyo Estévez <manuelhoyoestevez@gmail.com>
2021-03-15 18:33:47 +0100

    Working

--- node/src/index.js
+++ node/src/index.js
@@ -1,4 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
