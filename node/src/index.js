df7b8ee2e48002987606526e7ae576d2e88a48b3
Manuel Hoyo Estévez <manuelhoyoestevez@gmail.com>
2021-03-15 17:48:30 +0100

    Moved to subfolder

---
+++ node/src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<App />, app);
